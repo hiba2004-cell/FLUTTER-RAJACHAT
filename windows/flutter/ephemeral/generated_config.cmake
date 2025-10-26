dc48ab353ba415cc89e56a6bb2545c510b14a5ae
Nadiri hiba <hiba.nadiri04@gmail.com>
2025-10-26 15:10:09 +0100

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\USER\\OneDrive\\Desktop\\Flutter-Social-Media" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\USER\\OneDrive\\Desktop\\Flutter-Social-Media"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\USER\\OneDrive\\Desktop\\Flutter-Social-Media\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\USER\\OneDrive\\Desktop\\Flutter-Social-Media"
+  "FLUTTER_TARGET=C:\\Users\\USER\\OneDrive\\Desktop\\Flutter-Social-Media\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuNA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZDY5M2I0YjlkYg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YzI5ODA5MTM1MQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\USER\\OneDrive\\Desktop\\Flutter-Social-Media\\.dart_tool\\package_config.json"
+)
